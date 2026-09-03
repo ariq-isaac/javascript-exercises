54a8d41fc1229776ca11474f07cc60e704fa51c6
Ariq Isaac <2009ariq@gmail.com>
2026-09-03 16:16:49 +0800

    finish 07-reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let string = "";
+    let len = str.length - 1;
+    for (let i = len; i >= 0; i--) {
+        string += str[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
