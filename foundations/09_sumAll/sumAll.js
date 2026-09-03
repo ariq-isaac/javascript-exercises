58ecefed0515ef008f225489f14394cb6e41e76e
Ariq Isaac <2009ariq@gmail.com>
2026-09-03 17:01:27 +0800

    Finish 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Checks if a and b is a valid positive integer
+    if ((a < 0 || typeof a !== 'number' || !Number.isInteger(a)) || (b < 0 || typeof b !== 'number' || !Number.isInteger(b))) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    }
+    else if (b < a) {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
