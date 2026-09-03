7a6ccb9068856de09f6125ca833296725fd8885c
Ariq Isaac <2009ariq@gmail.com>
2026-09-03 17:33:55 +0800

    Finish 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let convert = (num - 32) * 5 / 9.0;
+  return Math.round(convert * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let convert = (num * 9/5) + 32;
+  return Math.round(convert * 10) / 10;
 };
 
 // Do not edit below this line
